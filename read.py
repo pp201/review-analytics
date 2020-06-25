f1cd9b745c686954b8bb5cb0165fb38e1f15b91c
Baron <popo333555@gmail.com>
2020-06-25 12:09:34 +0200

    calc avg left of review

--- read.py
+++ read.py
@@ -4,6 +4,13 @@ with open ('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 100000 == 0:
 			print(len(data))
+        
 print('The files are read, there are', count, 'data in total')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d) # sum_len = sum_len + len(d)
+	average = sum_len / len(data)
+print('The average length of message is', average)
